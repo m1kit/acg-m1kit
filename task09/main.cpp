378ebaf11d3856e678527c408775f0da542baf24
mikit <me@mikit.dev>
2022-07-06 04:31:47 +0900

    Complete task09

--- task09/main.cpp
+++ task09/main.cpp
@@ -4,6 +4,11 @@
 #include <pybind11/eigen.h>
 #include <pybind11/stl.h>
 #include <deque>
+#include <cmath>
+#include <numeric>
+#include <algorithm>
+
+// #define MIXED_SEAMLESS_CLONING
 
 namespace py = pybind11;
 
@@ -24,6 +29,7 @@ Eigen::MatrixXd CppPoissonBlending(
     unsigned int num_iteration){
   assert(src.cols() == src_mask.cols() );
   assert(src.cols() == src_mask.cols() );
+  Eigen::MatrixXd org = dist.cast<double>();
   Eigen::MatrixXd ret = dist.cast<double>();
   for(unsigned int itr=0;itr<num_iteration;++itr) {  // Gauss-Seidel iteration
     for (unsigned int src_i = 1; src_i < src.rows() - 1; ++src_i) {
@@ -33,17 +39,42 @@ Eigen::MatrixXd CppPoissonBlending(
         unsigned int ret_j = src_j + offset[1];
         if (ret_i <= 0 || ret_i >= dist.rows() - 1) { continue; }
         if (ret_j <= 0 || ret_j >= dist.cols() - 1) { continue; }
-        const double src_n = src(src_i, src_j+1); // north
-        const double src_s = src(src_i, src_j-1); // south
-        const double src_e = src(src_i+1, src_j); // east
-        const double src_w = src(src_i-1, src_j); // west
+        // src_[nsew]: aka g_q, src_c: aka g_p
+        const double src_nsew[4] = {
+          static_cast<double>(src(src_i, src_j+1)), // north
+          static_cast<double>(src(src_i, src_j-1)), // south
+          static_cast<double>(src(src_i+1, src_j)), // east
+          static_cast<double>(src(src_i-1, src_j)), // west
+        };
         const double src_c = src(src_i, src_j); // center
-        const double ret_n = ret(ret_i, ret_j+1);
-        const double ret_s = ret(ret_i, ret_j-1);
-        const double ret_e = ret(ret_i+1, ret_j);
-        const double ret_w = ret(ret_i-1, ret_j);
+        // ret_nsew: aka f_q, ret_c: aka f_p
+        const double ret_nsew[4] = {
+          static_cast<double>(ret(ret_i, ret_j+1)), // north
+          static_cast<double>(ret(ret_i, ret_j-1)), // south
+          static_cast<double>(ret(ret_i+1, ret_j)), // east
+          static_cast<double>(ret(ret_i-1, ret_j)), // west
+        };
         // write some code here to implement Poisson image editing
-        double ret_c = src_c; // change this line
+#ifndef MIXED_SEAMLESS_CLONING
+        // 1. seamless cloning
+        double ret_c = src_c + (std::accumulate(ret_nsew, ret_nsew + 4, 0.0) - std::accumulate(src_nsew, src_nsew + 4, 0.0)) / 4.0;
+#else
+        // 2. mixed seamless cloning
+        // org_nsew: aka f^*_q, org_c: aka f^*_p
+        const double org_nsew[4] = {
+          static_cast<double>(org(ret_i, ret_j+1)), // north
+          static_cast<double>(org(ret_i, ret_j-1)), // south
+          static_cast<double>(org(ret_i+1, ret_j)), // east
+          static_cast<double>(org(ret_i-1, ret_j)), // west
+        };
+        const double org_c = org(ret_i, ret_j);
+        double ret_c = std::accumulate(ret_nsew, ret_nsew + 4, 0.0) / 4.0;
+        for (int i = 0; i < 4; i++) {
+          const double df = (org_c - org_nsew[i]) / 4.0;
+          const double dg = (src_c - src_nsew[i]) / 4.0;
+          ret_c += std::abs(df) > std::abs(dg) ? df : dg;
+        }
+#endif
         // no edit below
         ret_c = (ret_c>255.) ? 255. : ret_c; // clamp
         ret_c = (ret_c<0.) ? 0. : ret_c; // clamp
